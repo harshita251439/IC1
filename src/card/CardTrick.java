e9754c87afdf9d7bbe88b2cdc7961bce1654f324
harshita251439 <157140787+harshita251439@users.noreply.github.com>
2024-02-04 21:36:22 -0500

    Update CardTrick.java
    add luckyCard

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -22,6 +22,9 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+         Card luckyCard = new Card();
+        luckyCard.setSuit("Hearts");
+        luckyCard.setValue(7);
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
